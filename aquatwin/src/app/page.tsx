09dc5200942da988028810d16b392d53cc95992f
Peachy-Njenga <peacheswagithi16@gmail.com>
2024-05-13 17:42:26 +0300

    Set up repository

---
+++ aquatwin/src/app/page.tsx
@@ -0,0 +1,22 @@
+import Contact from "@/components/contact";
+import Footer from "@/components/footer";
+import Hero from "@/components/hero";
+import Patners from "@/components/patners";
+import Problem from "@/components/problem";
+import Solution from "@/components/solution";
+import Team from "@/components/team";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Hero />
+      <Problem/>
+      <Solution/>
+      <Team />
+      <Patners />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
---
+++ aquatwin/src/components/hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='w-screen h-screen text-white bg-black'>
+      Hero
+    </div>
+  )
+}
+
+export default Hero
