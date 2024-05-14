f53422aa3b739dec2ba967cc5bbdb5b46d1dfd85
Peachy-Njenga <peacheswagithi16@gmail.com>
2024-05-14 19:45:38 +0300

    Contact Form with validation

--- aquatwin/src/components/contact.tsx
+++ aquatwin/src/components/contact.tsx
@@ -1,9 +1,123 @@
+"use client"
 import React from 'react'
 
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { toast } from "@/components/ui/use-toast"
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(1, {
+      message: "This field is required"
+    })
+    .max(50, {
+      message: "Name can not exceed more than 50  characters"
+    }),
+  email: z
+    .string()
+    .email(),
+  userMessage: z
+    .string().min(1, {
+      message: "This field is required"
+    })
+    .max(1000, {
+      message: "Word Limit exceeded"
+    }),
+
+})
+
+
 const Contact = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      userMessage: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+        </pre>
+      ),
+    })
+    console.log(values)
+  }
+
   return (
-    <div>
-      Contact us
+    <div className='bg-primary-1 w-screen h-screen p-5 flex flex-col gap-5 px-10 md:px-80'>
+      <h1 className='text-4xl text-center font-bold '>Get in Touch</h1>
+      <p className='text-sm md:text:md text-center'>We'd love to hear from you. Please reach out to us for any inquiries or collaborations.</p>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className=" flex flex-col gap-7 md:px-20 mt-5 ">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="userMessage"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Message</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder=""
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+        <div className='mx-auto'>
+          <Button variant="submit" type="submit">Submit</Button>
+        </div>
+      </Form>
     </div>
   )
 }
