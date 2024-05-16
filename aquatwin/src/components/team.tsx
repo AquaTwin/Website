7f7f30f0b7dbd9f2d19be2447b8f18bf73002fc4
Javan Otieno <javan.o.otieno@gmail.com>
2024-05-16 10:33:27 +0300

    feat(app): add team section

--- aquatwin/src/components/team.tsx
+++ aquatwin/src/components/team.tsx
@@ -1,10 +1,63 @@
 import React from 'react'
+import TeamCard from './ui/team-card'
+import TestImage  from '../public/next.svg'
 
 const Team = () => {
+  const teamMembers = [
+    {
+    image: '/image.png',
+    name: 'John Doe',
+    title: 'Frontend Lead',
+    github: 'https://github.com',
+    linkedIn: 'https://linkedin.com'
+  },
+  {
+    image: '/image.png',
+    name: 'John Doe',
+    title: 'Frontend Lead',
+    github: 'https://github.com',
+    linkedIn: 'https://linkedin.com'
+  },
+  {
+    image: '/image.png',
+    name: 'John Doe',
+    title: 'Frontend Lead',
+    github: 'https://github.com',
+    linkedIn: 'https://linkedin.com'
+  },
+  {
+    image: '/image.png',
+    name: 'John Doe',
+    title: 'Frontend Lead',
+    github: 'https://github.com',
+    linkedIn: 'https://linkedin.com'
+  },
+  {
+    image: '/image.png',
+    name: 'John Doe',
+    title: 'Frontend Lead',
+    github: 'https://github.com',
+    linkedIn: 'https://linkedin.com'
+  },
+]
+
   return (
-    <div>
-      
-    </div>
+    <section className='space-y-8 w-full max-w-[1400px] mx-auto py-16'>
+      <div className='space-y-2 text-center'>
+      <p className="text-text font-bold text-sm text-center mb-3">
+        Experienced
+      </p>
+        <h2 className='text-4xl font-bold'>Meet Our Team</h2>
+        <p className='md:text-lg'>Our team is made up of passionate individuals who are dedicated to making a difference in the world.</p>
+      </div>
+      <div className='flex flex-wrap justify-start w-full gap-5'>
+        {teamMembers.map((team, index) => (
+          <TeamCard key={index} {...team}/>
+        ))}
+      </div>
+
+    </section>
+
   )
 }
 
