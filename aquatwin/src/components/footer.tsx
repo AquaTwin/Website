6ff5bc0088172a860690c6a7233f480e21be4f5b
Javan Otieno <javan.o.otieno@gmail.com>
2024-05-16 10:32:45 +0300

    feat(app): add footer

--- aquatwin/src/components/footer.tsx
+++ aquatwin/src/components/footer.tsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import { Separator } from "@/components/ui/separator"
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Footer = () => {
+  const links = [
+    'home',
+    'problem',
+    'solution',
+    'team',
+    'contact'
+  ]
+
   return (
-    <div>
-      
-    </div>
+    <section className='text-center p-4 bg-primary-1'>
+      <div className='mb-3'>
+        <Image alt='logo' src="/aquatwin-logo.png" height={200} width={200} className='mx-auto'/>
+        <div className='mt-7'>
+          <ul className='flex flex-col sm:flex-row sm:justify-center gap-3 font-medium'>
+            {links.map((link, index) => (
+              <li key={index}>
+                <Link className='hover:text-btn hover:underline' href={`#{link}`}>
+                  {link.toUpperCase()}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      {/* <Separator /> */}
+      <hr className='fill-white bg-title block h-[2px]'/>
+      <footer className='mt-3'>
+        <div className='text-sm underline flex flex-col sm:flex-row sm:justify-center gap-2 sm:gap-5'>
+          <Link href="#" >
+            Privacy policy
+          </Link>
+          <Link href="#" >
+            Terms of Service
+          </Link>
+        </div>
+        <div className='mt-3 text-sm'>
+          <p>&copy; 2024. All rights reserved.</p>
+        </div>
+      </footer>
+    </section>
   )
 }
 
 export default Footer
+
+
