867d2ab56f75c24363444b1015ea4b586ea314d7
Peachy-Njenga <peacheswagithi16@gmail.com>
2024-05-20 10:35:30 +0300

    Added about us page

--- aquatwin/src/app/page.tsx
+++ aquatwin/src/app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/about";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
@@ -7,10 +8,12 @@ import Solution from "@/components/solution";
 import Team from "@/components/team";
 import Image from "next/image";
 
+
 export default function Home() {
   return (
     <main className="flex flex-col bg-primary-1 ">
       <Hero />
+      <About />
       <Problem/>
       <Solution/>
       <Team />
---
+++ aquatwin/src/components/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='bg-primary-1 py-16 px-10'>
+            <h2 className="text-4xl font-bold text-title text-center mb-5">About us</h2>
+            <div className="flex flex-wrap justify-center items-center gap-6 mt-5 ">
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default About
