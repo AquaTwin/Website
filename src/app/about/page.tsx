1462bd06cf6dd2c70839f90e3253b052a2e6b071
Javan Otieno <javan.o.otieno@gmail.com>
2024-05-28 11:58:59 +0300

    fix(app): fix challenge carousel images

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,17 +17,17 @@ import {
 
 const AboutPage = () => {
     return (
-        <main>
+        <main className='bg-primary-1'>
             <Header />
-            <h1>About AQUATWIN</h1>
+            <h1 className='text-center text-title text-5xl font-semibold'>About AQUATWIN</h1>
             <section>
                 <Container>
                     <div className='md:flex gap-7'>
                         <div className=''>
-                            <Image src='/river-basin.jpg' alt="digital twin" width="900" height="400" className='w-full'></Image>
+                            <Image src='/digital-twin.jpeg' alt="digital twin" width="900" height="400" className='w-full'></Image>
                         </div>
                         <div>
-                            <h2>What is a digital twin</h2>
+                            <h2 className='text-title text-3xl font-semibold'>What is a digital twin</h2>
                             <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, a. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur eaque cupiditate vero dolore atque? Laboriosam omnis aperiam eaque, libero fuga sequi molestiae sed error molestias quidem officia quasi exercitationem non.</p>
                         </div>
 
@@ -36,7 +36,7 @@ const AboutPage = () => {
             </section>
             <section>
                 <Container >
-                    <div className='relative gap-7 mx-auto max-h-[500px] overflow-clip'>
+                    <div className='relative gap-7 mx-auto max-h-[450px] lg:max-h-[600px] overflow-clip'>
 
                         <div className='absolute flex flex-col items-center justify-center z-10 bg-[#37373799] text-white w-full h-full'>
                             <h2>Challenges</h2>
@@ -58,16 +58,16 @@ const AboutPage = () => {
                             >
                                 <CarouselContent className=''>
 
-                                    <CarouselItem className="w-full ">
-                                        <Image className='object-cover w-full ' alt='river usage' width={400} height={400} src="/water-usage.jpeg"></Image>
+                                    <CarouselItem className="">
+                                        <Image className='object-none object-center h-full w-auto  mx-auto' alt='river usage' width={400} height={400} src="/flooded-river.jpeg"></Image>
                                     </CarouselItem>
 
                                     <CarouselItem className="">
-                                        <Image className='object-cover w-full' alt='river usage ' width={400} height={400} src="/destroyed-banks1.jpeg"></Image>
+                                        <Image className='object-cover h-full w-auto' alt='river usage ' width={400} height={400} src="/destroyed-banks1.jpeg"></Image>
                                     </CarouselItem>
 
                                     <CarouselItem className="">
-                                        <Image className='object-cover w-full' alt='river usage' width={400} height={400} src="/flooded-river.jpeg"></Image>
+                                        <Image className='h-full w-auto object-cover' alt='river usage' width={400} height={400} src="/flooded-house.jpeg"></Image>
                                     </CarouselItem>
 
                                 </CarouselContent>
@@ -79,7 +79,15 @@ const AboutPage = () => {
 
             <section>
                 <Container>
-                    <h2 className='text-center'>River basins of interest</h2>
+                    <h2 className='text-center text-title text-3xl font-semibold'>Our solution</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil tempora nobis, ipsum laudantium dolorum quasi Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, libero deleniti fugiat dolorum ex aspernatur molestias possimus commodi voluptate! Natus velit dicta dolor quidem accusantium! commodi ducimus rem obcaecati voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, eius facere. Similique, fuga voluptatibus dicta non itaque illum necessitatibus impedit.</p>
+
+                </Container>
+            </section>
+
+            <section>
+                <Container>
+                    <h2 className='text-center text-title text-3xl font-semibold'>River basins of interest</h2>
                     <div className='flex flex-wrap justify-around'>
                         <h2>Omo ---- River basin</h2>
                         <h2>Omo ---- River basin</h2>
@@ -92,13 +100,6 @@ const AboutPage = () => {
                 </Container>
             </section>
 
-            <section>
-                <Container>
-                    <h2 className='text-center'>Our solution</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil tempora nobis, ipsum laudantium dolorum quasi Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, libero deleniti fugiat dolorum ex aspernatur molestias possimus commodi voluptate! Natus velit dicta dolor quidem accusantium! commodi ducimus rem obcaecati voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, eius facere. Similique, fuga voluptatibus dicta non itaque illum necessitatibus impedit.</p>
-
-                </Container>
-            </section>
             <Footer />
         </main>
     )
