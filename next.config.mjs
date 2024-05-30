370f909716e7d9f882494a1e4921c12bbfeb009b
Sandrah <comfortsandrah13@gmail.com>
2024-05-30 13:51:27 +0300

    Remove dist

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
     // skipTrailingSlashRedirect: true,
 
     // Optional: Change the output directory `out` -> `dist`
-    distDir: 'dist',
+    // distDir: 'dist',
 
     images: {
         unoptimized: true,
